c8fcb9b55b317fcf7baa7011894d698477c1bf51
Vishal Avalani <vavalani@affirma.com>
2018-05-06 15:56:11 +0530

    ref and refs!

--- public/app.js
+++ public/app.js
@@ -5,15 +5,25 @@ var Welcome = React.createClass({
       state: "Maharashtra"
     };
   },
+  callme: function(e) {
+    e.preventDefault();
+    const city = this.refs.txtCity.value;
+    alert(city);
+    alert("I am called");
+  },
   render() {
     const mycity = this.props.city;
     const mystate = this.props.state;
     return (
-      <div>
+      <div className="container">
         <h1>Welcome Component</h1>
         <p>This is my first component</p>
         <p>My city is: {mycity}</p>
         <p>My state is: {mystate}</p>
+        <form onSubmit={this.callme}>
+          <input type="text" ref="txtCity" />
+          <button className="btn btn-default">call me</button>
+        </form>
       </div>
     );
   }
