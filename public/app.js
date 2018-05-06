9c5cf49163cec1eb94eac4cccf665467238fd27d
Vishal Avalani <vavalani@affirma.com>
2018-05-06 15:36:16 +0530

    default props

--- public/app.js
+++ public/app.js
@@ -1,12 +1,22 @@
 var Welcome = React.createClass({
+  getDefaultProps: function() {
+    return {
+      city: "Pune",
+      state: "Maharashtra"
+    };
+  },
   render() {
+    const mycity = this.props.city;
+    const mystate = this.props.state;
     return (
       <div>
         <h1>Welcome Component</h1>
         <p>This is my first component</p>
+        <p>My city is: {mycity}</p>
+        <p>My state is: {mystate}</p>
       </div>
     );
   }
 });
 
-ReactDOM.render(<Welcome />, document.getElementById("app"));
+ReactDOM.render(<Welcome state="Gujarat" />, document.getElementById("app"));
