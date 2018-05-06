95c9b192e0cccbac33138b8b2f6152cfe9e7a7c8
Vishal Avalani <vavalani@affirma.com>
2018-05-06 16:30:00 +0530

    basics of state

--- public/app.js
+++ public/app.js
@@ -5,23 +5,37 @@ var Welcome = React.createClass({
       state: "Maharashtra"
     };
   },
+  getInitialState: function() {
+    return {
+      name: "Narendra"
+    };
+  },
   callme: function(e) {
     e.preventDefault();
     const city = this.refs.txtCity.value;
-    alert(city);
+    const name = this.refs.txtName.value;
+    this.setState({
+      name: name
+    });
+    //alert(city);
     alert("I am called");
   },
   render() {
     const mycity = this.props.city;
     const mystate = this.props.state;
+    const myname = this.state.name;
     return (
       <div className="container">
         <h1>Welcome Component</h1>
         <p>This is my first component</p>
         <p>My city is: {mycity}</p>
         <p>My state is: {mystate}</p>
+        <p>My name is: {myname}</p>
         <form onSubmit={this.callme}>
           <input type="text" ref="txtCity" />
+          <br />
+          Enter your name <br />
+          <input type="text" ref="txtName" />
           <button className="btn btn-default">call me</button>
         </form>
       </div>
