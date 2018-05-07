636eb13bcf307210f03cb1e59d1200c0a18a8cd7
Vishal Avalani <vavalani@affirma.com>
2018-05-07 11:45:43 +0530

    get weather data from API

--- public/components/welcomemain.js
+++ public/components/welcomemain.js
@@ -1,20 +1,22 @@
 var React = require("react");
-
 var WelcomeMessage = require("./welcomemessage.js");
 var WelcomeForm = require("./welcomeform.js");
+var openWeatherMap = require("./api/openweathermap.js");
 
 var WelcomeMain = React.createClass({
-  getDefaultProps() {
-    return {
-      city: "Ahmedabad"
-      // state: "GUJ",
-      // name: "Vishal",
-      // email: "vishal.avalani@gmail.com"
-    };
-  },
+  // getDefaultProps() {
+  //   return {
+  //     city: "Ahmedabad"
+  //     // state: "GUJ",
+  //     // name: "Vishal",
+  //     // email: "vishal.avalani@gmail.com"
+  //   };
+  // },
   getInitialState() {
     return {
-      city: this.props.city
+      // city: this.props.city,
+      city: "Ahmedabad",
+      temp: 44
       // state: this.props.state,
       // name: this.props.name,
       // email: this.props.email
@@ -24,12 +26,30 @@ var WelcomeMain = React.createClass({
   //   this.setState(updates);
   // },
   updateState(city) {
-    this.setState({
-      city: city
-    });
+    var that = this;
+
+    openWeatherMap.getTemp(city).then(
+      function(temp) {
+        that.setState({
+          city: city,
+          temp: temp
+        });
+      },
+      function(errorMessage) {
+        alert(errorMessage);
+      }
+    );
+
+    // this.setState({
+    //   city: city
+    // });
   },
   render() {
-    const city = this.state.city;
+    // const city = this.state.city;
+    // const temp = this.state.temp;
+
+    const { city, temp } = this.state;
+
     // const state = this.state.state;
     // const name = this.state.name;
     // const email = this.state.email;
@@ -38,6 +58,7 @@ var WelcomeMain = React.createClass({
       <div className="container">
         <WelcomeMessage
           city={city}
+          temp={temp}
           // state={state} name={name} email={email}
         />
         <WelcomeForm collectData={this.updateState} />
--- public/components/welcomemessage.js
+++ public/components/welcomemessage.js
@@ -2,14 +2,26 @@ var React = require("react");
 
 var WelcomeMessage = React.createClass({
   render() {
-    const city = this.props.city;
-    const state = this.props.state;
-    const name = this.props.name;
-    const email = this.props.email;
+    const { city, temp } = this.props;
+    // const city = this.props.city;
+    // const temp = this.props.temp;
+    // const state = this.props.state;
+    // const name = this.props.name;
+    // const email = this.props.email;
 
     return (
       <div>
-        <h3>city: {city}</h3>
+        <div className="panel panel-default panel-primary">
+          <div className="panel-heading">OpenWeather Temperature</div>
+          <div className="panel-body">
+            <h3>
+              <strong>City:</strong> {city}
+            </h3>
+            <h3>
+              <strong>Temperataure:</strong> {temp} C
+            </h3>
+          </div>
+        </div>
         {/* <h3>state: {state}</h3>
         <h3>name: {name}</h3>
         <h3>email: {email}</h3> */}
